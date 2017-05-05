d926e783509f9746e0fd24cd881d95371bff9708
Toshimitsu Yamaguchi <toshi.yamaguchi@eng.niigata-u.ac.jp>
2017-05-05 18:02:50 +0900

    振動モータの制御まで

--- VoiceMonitor.ino
+++ VoiceMonitor.ino
@@ -1,5 +1,8 @@
 const int sampleWindow = 250; // Sample window width in mS (250 mS = 4Hz)
 unsigned int inputSignal;
+unsigned int inputSignals[] = {0, 0, 0};
+
+
 
 void setup() {
   Serial.begin(9600);
@@ -13,8 +16,6 @@ void loop() {
  unsigned int signalMin = 1024;
 
  // collect data for 250 miliseconds
- unsigned long sum = 0;
- unsigned long cnt = 0;
  while (millis() - start < sampleWindow){
   inputSignal = analogRead(A0);
 
@@ -26,16 +27,34 @@ void loop() {
       signalMin = inputSignal;  // save just the min levels
     }
   }
-
-  sum += inputSignal;
   
  }
  
  peakToPeak = signalMax - signalMin;  // max - min = peak-peak amplitude
- double volts = (peakToPeak * 3.3) / 1024;  // convert to volts
+ //double volts = (peakToPeak * 3.3) / 1024;  // convert to volts
+
+ inputSignals[2] = inputSignals[1];
+ inputSignals[1] = inputSignals[0];
+ inputSignals[0] = peakToPeak;
+
+ if(avg() > analogRead(A1)){
+   Serial.println("1");
+   analogWrite( 9, 80 );
+ } else {
+   Serial.println("0");  
+   analogWrite( 9, 0 );
+ }
  
+ //Serial.println(peakToPeak);
  //Serial.println(volts);
- Serial.println(sum/cnt);
  //Serial.println(analogRead(A1));
 
+}
+
+unsigned int avg(){
+  unsigned int sum = 0;
+  for(int i = 0; i < 3; ++i){
+    sum += inputSignals[i];
+  }
+  return sum/3;
 }
